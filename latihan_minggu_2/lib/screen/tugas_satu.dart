0a8240459d9c3ae13f3618c281779b05d98e1529
WIDARSONO\widarsono <rizkawidarsono29@gmail.com>
2021-11-23 22:15:16 +0700

    tugas satu

--- latihan_minggu_2/lib/screen/tugas_satu.dart
+++ latihan_minggu_2/lib/screen/tugas_satu.dart
@@ -5,6 +5,38 @@ class TugasSatu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        leading: Builder(
+          builder: (BuildContext context) {
+            return const FlutterLogo(
+              size: 2,
+            );
+          },
+        ),
+        title: const Text('Text Judul'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.ac_unit),
+          ),
+        ],
+      ),
+      body: const BodyTugasSatu(),
+    );
+  }
+}
+
+class BodyTugasSatu extends StatelessWidget {
+  const BodyTugasSatu({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Hello World',
+        style: TextStyle(fontSize: 30),
+      ),
+    );
   }
 }
